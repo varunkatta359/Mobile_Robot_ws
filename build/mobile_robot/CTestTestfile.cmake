86e98075e57a576e02e416b80a87a9c8f2eacc81
Varun Kumar Katta <varunkatta123@gmail.com>
2024-04-07 16:20:28 +0530

    a

---
+++ build/mobile_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/varun/project_ws/src/mobile_robot
+# Build directory: /home/varun/project_ws/build/mobile_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ install/mobile_robot/share/mobile_robot/local_setup.zsh
@@ -0,0 +1,1 @@
+/home/varun/project_ws/build/mobile_robot/ament_cmake_environment_hooks/local_setup.zsh
